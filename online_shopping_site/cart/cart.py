efe37e8baf222118b4717936ef34fbc007dfa577
alindafortunate <alindafortunate5@gmail.com>
2025-09-25 16:02:33 +0300

    Closure for 25/9/2025 day session due to power shortage

--- online_shopping_site/cart/cart.py
+++ online_shopping_site/cart/cart.py
@@ -5,7 +5,9 @@ from shop.models import Product
 
 class Cart:
     def __init__(self, request):
-        """Initialize the cart"""
+        """
+        Initialize the cart
+        """
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
