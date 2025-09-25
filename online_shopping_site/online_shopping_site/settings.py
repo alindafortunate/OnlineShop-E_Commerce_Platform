0edd97443be466195899081c624dc19956a8cb5b
alindafortunate <alindafortunate5@gmail.com>
2025-09-25 15:52:55 +0300

    Created the cart app to managing the shopping functionality

--- online_shopping_site/online_shopping_site/settings.py
+++ online_shopping_site/online_shopping_site/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "shop",
+    "cart.apps.CartConfig",
+    "shop.apps.ShopConfig",
 ]
 
 MIDDLEWARE = [
