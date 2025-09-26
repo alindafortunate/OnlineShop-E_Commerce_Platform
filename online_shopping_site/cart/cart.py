47b941d82c5ca31354f295873103c008ceab7909
alindafortunate <alindafortunate5@gmail.com>
2025-09-26 12:42:20 +0300

    Try to understand the add() method for the cart functionality

--- online_shopping_site/cart/cart.py
+++ online_shopping_site/cart/cart.py
@@ -9,8 +9,24 @@ class Cart:
         Initialize the cart
         """
         self.session = request.session
+        # Getting the cart from the current session.
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
             # Save empty cart in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+
+    def add(self, product, quantity=1, override_quantity=False):
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {
+                "quantity": 0,
+                "price": str(product.price),
+            }
+        if override_quantity:
+            self.cart[product_id]["quantity"] = quantity
+        else:
+            self.cart[product_id]["quantity"] += quantity
+
+    def save(self):
+        self.session.modified = True
