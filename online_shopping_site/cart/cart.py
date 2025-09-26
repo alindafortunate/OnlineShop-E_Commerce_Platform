8346668563fd7df5aa86f59d6fa78dc31f1143df
alindafortunate <alindafortunate5@gmail.com>
2025-09-26 13:00:59 +0300

    Coming closer to the understanding of why override_quantity is vital

--- online_shopping_site/cart/cart.py
+++ online_shopping_site/cart/cart.py
@@ -17,6 +17,9 @@ class Cart:
         self.cart = cart
 
     def add(self, product, quantity=1, override_quantity=False):
+        """
+        Add a product to the cart or update its quantity.
+        """
         product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id] = {
@@ -29,4 +32,5 @@ class Cart:
             self.cart[product_id]["quantity"] += quantity
 
     def save(self):
+        # Mark the session as modified to make sure it gets saved.
         self.session.modified = True
