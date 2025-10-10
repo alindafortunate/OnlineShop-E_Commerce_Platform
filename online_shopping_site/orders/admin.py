2517a162e2dccc706f14d21546bd58a5da9eb0e6
alindafortunate <alindafortunate5@gmail.com>
2025-10-10 18:35:28 +0300

    Understood the use of TabularInline especially when you have a parent and a child model or your to display the two models on one page.

--- online_shopping_site/orders/admin.py
+++ online_shopping_site/orders/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ["product"]
+
+
+@admin.register(Order)
+class OrderAdmin(ModelAdmin):
+    list_display = ["id", "first_name", "last_name", "email", "address", "city"]
+    list_filter = ["created", "paid", "updated"]
+    inlines = [OrderItemInline]
