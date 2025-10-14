5969ab7ea3c3668c01e64812f5c8085720037222
alindafortunate <alindafortunate5@gmail.com>
2025-10-14 15:27:46 +0300

    Almost done with configuring the order option.

--- online_shopping_site/orders/views.py
+++ online_shopping_site/orders/views.py
@@ -10,13 +10,13 @@ def order_create(request):
         form = OrderCreateForm(request.POST)
         if form.is_valid():
             order = form.save()
-        for item in cart:
-            OrderItem.objects.create(
-                order=order,
-                product=item["product"],
-                price=item["price"],
-                quantity=item["quantity"],
-            )
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item["product"],
+                    price=item["price"],
+                    quantity=item["quantity"],
+                )
         # clear the cart.
         cart.clear()
         return render(request, "orders/order/created.html", {"order": order})
