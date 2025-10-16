caa6e1c5b990ca2a81026389503b49d110d94a09
alindafortunate <alindafortunate5@gmail.com>
2025-10-16 16:44:31 +0300

    Created a task for sending an e-mail notification when an order is placed.

---
+++ online_shopping_site/orders/tasks.py
@@ -0,0 +1,16 @@
+from celery import shared_task
+from django.core.mail import send_mail
+from .models import Order
+
+
+def order_create(order_id):
+    """
+    Task to send an email notification when an order is successfully created.
+    """
+    order = Order.objects.get(id=order_id)
+    subject = f"Order number:{order.id}"
+    message = f"Dear {order.first_name}, thank your placing an order. \n\n Your order {order.id} was successful."
+    mail_sent = send_mail(
+        subject, message, "admin@online_shopping_site.com", [order.email]
+    )
+    return mail_sent
