97cad694092f6a4edd95f674dd83a8d8ce16272c
alindafortunate <alindafortunate5@gmail.com>
2025-10-16 12:28:28 +0300

    Installed Celery to act as a message worker.

--- online_shopping_site/orders/views.py
+++ online_shopping_site/orders/views.py
@@ -17,8 +17,8 @@ def order_create(request):
                     price=item["price"],
                     quantity=item["quantity"],
                 )
-        # clear the cart.
-        cart.clear()
+            # clear the cart.
+            cart.clear()
         return render(request, "orders/order/created.html", {"order": order})
     else:
         form = OrderCreateForm()
