20354c449a8564674387f0eb2f8051a222e3ed10
alindafortunate <alindafortunate5@gmail.com>
2025-10-16 17:08:53 +0300

    Added email configuration settings.

--- online_shopping_site/online_shopping_site/celery.py
+++ online_shopping_site/online_shopping_site/celery.py
@@ -1,7 +1,8 @@
 import os
 from celery import Celery
 
+# Set the default Django settings module for the 'celery' program
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "online_shopping_site.settings")
 app = Celery("online_shopping_site")
-app.config_from_object("django.config:settings", namespace="CELERY")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
--- online_shopping_site/orders/tasks.py
+++ online_shopping_site/orders/tasks.py
@@ -3,6 +3,7 @@ from django.core.mail import send_mail
 from .models import Order
 
 
+@shared_task
 def order_create(order_id):
     """
     Task to send an email notification when an order is successfully created.
@@ -11,6 +12,9 @@ def order_create(order_id):
     subject = f"Order number:{order.id}"
     message = f"Dear {order.first_name}, thank your placing an order. \n\n Your order {order.id} was successful."
     mail_sent = send_mail(
-        subject, message, "admin@online_shopping_site.com", [order.email]
+        subject,
+        message,
+        "alindafortunate5@gmail.com",
+        [order.email],
     )
     return mail_sent
