25c5756bec98e09d5e6bef00917c4d8bc013b80e
alindafortunate <alindafortunate5@gmail.com>
2025-10-18 21:33:25 +0300

    Changed ALLOWED_HOSTS

--- online_shopping_site/online_shopping_site/settings.py
+++ online_shopping_site/online_shopping_site/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "https://online-shopping-site-jr3o.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
