e85e77abf7232088defa32629b59ef64540451e6
alindafortunate <alindafortunate5@gmail.com>
2025-10-22 08:55:53 +0300

    Switched back to local database to handle the payment and order functionality.

--- online_shopping_site/online_shopping_site/settings.py
+++ online_shopping_site/online_shopping_site/settings.py
@@ -84,16 +84,16 @@ WSGI_APPLICATION = "online_shopping_site.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-# Online DB
 DATABASES = {
-    "default": dj_database_url.parse(config("External_Database_URL")),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+# Online DB
+# DATABASES = {
+#     "default": dj_database_url.parse(config("External_Database_URL")),
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
