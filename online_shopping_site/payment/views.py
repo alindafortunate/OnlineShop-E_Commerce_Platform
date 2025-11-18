3cda12e126050c44de4762154f0ff4fd41c65bf7
alindafortunate <alindafortunate5@gmail.com>
2025-11-18 11:09:08 +0300

    Edited the payment_process view.

--- online_shopping_site/payment/views.py
+++ online_shopping_site/payment/views.py
@@ -15,7 +15,7 @@ def payment_process(request):
     order = get_object_or_404(Order, id=order_id)
     if request.method == "POST":
         success_url = request.build_absolute_uri(reverse("payment:completed"))
-        cancel_url = request.build_absolute_uri(request("payment:canceled"))
+        cancel_url = request.build_absolute_uri(reverse("payment:canceled"))
         # Stripe session checkout data
         session_data = {
             "mode": "payment",
