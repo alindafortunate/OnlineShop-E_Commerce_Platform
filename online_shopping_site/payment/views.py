8c595790ce7d5dbf9fa1dec945690bfaa86b7e49
alindafortunate <alindafortunate5@gmail.com>
2025-11-19 15:29:17 +0300

    Integrated stripe payment, and its settings on the payment_process view up

--- online_shopping_site/payment/views.py
+++ online_shopping_site/payment/views.py
@@ -47,7 +47,7 @@ def payment_process(request):
 
 
 def payment_completed(request):
-    return render(request, "payment.completed.html")
+    return render(request, "payment/completed.html")
 
 
 def payment_canceled(request):
