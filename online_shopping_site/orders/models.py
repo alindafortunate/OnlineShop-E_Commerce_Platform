a6861a35bb0e766a750a39faf0160e175bd51694
alindafortunate <alindafortunate5@gmail.com>
2025-11-26 12:37:07 +0300

    Added a stripe_id field to the Order model to be able to pick the stripe payment id.

--- online_shopping_site/orders/models.py
+++ online_shopping_site/orders/models.py
@@ -11,6 +11,7 @@ class Order(models.Model):
     paid = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    stripe_id = models.CharField(max_length=250, blank=True)
 
     class Meta:
         ordering = ["-created"]
