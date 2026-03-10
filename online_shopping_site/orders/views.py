89e4fc6a64de3fbc2c9a8f1f7b499976043b25f6
alindafortunate <alindafortunate5@gmail.com>
2026-03-10 18:09:34 +0300

    Worked with tables in HTML Django.

--- online_shopping_site/orders/views.py
+++ online_shopping_site/orders/views.py
@@ -1,6 +1,6 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import get_object_or_404, redirect, render
-from .models import OrderItem
+from .models import Order, OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
@@ -41,5 +41,5 @@ def order_create(request):
 
 @staff_member_required
 def admin_order_detail(request, order_id):
-    order = get_object_or_404(OrderItem, id=order_id)
+    order = get_object_or_404(Order, id=order_id)
     return render(request, "admin/orders/order/detail.html", {"order": order})
