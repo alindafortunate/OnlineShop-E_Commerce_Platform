ecf1330117ae3fda8f81dc4f74489c57da0bff7d
alindafortunate <alindafortunate5@gmail.com>
2026-04-17 14:03:06 +0300

    Worked on translation for the cart and coupon forms.

--- online_shopping_site/coupons/models.py
+++ online_shopping_site/coupons/models.py
@@ -2,6 +2,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
+
 class Coupon(models.Model):
     code = models.CharField(max_length=50, unique=True)
     discount = models.IntegerField(
