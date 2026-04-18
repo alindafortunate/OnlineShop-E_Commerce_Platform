ff67360858af98283e803251338c76d93083a9b2
alindafortunate <alindafortunate5@gmail.com>
2026-04-18 20:43:44 +0300

    Marked the main urls for translation

--- online_shopping_site/online_shopping_site/urls.py
+++ online_shopping_site/online_shopping_site/urls.py
@@ -17,17 +17,19 @@ Including another URLconf
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import gettext_lazy as _
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path("admin/", admin.site.urls),
-    path("cart/", include("cart.urls", namespace="cart")),
-    path("orders/", include("orders.urls", namespace="orders")),
-    path("payments/", include("payment.urls", namespace="payment")),
-    path("coupons/", include("coupons.urls", namespace="coupons")),
+    path(_("cart/"), include("cart.urls", namespace="cart")),
+    path(_("orders/"), include("orders.urls", namespace="orders")),
+    path(_("payments/"), include("payment.urls", namespace="payment")),
+    path(_("coupons/"), include("coupons.urls", namespace="coupons")),
     path("rosetta/", include("rosetta.urls")),
     path("", include("shop.urls", namespace="shop")),
-]
+)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
