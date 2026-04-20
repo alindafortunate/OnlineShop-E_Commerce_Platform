91a53d5adc0ad90ef28fe05f6f0186693dfd2b92
alindafortunate <alindafortunate5@gmail.com>
2026-04-20 12:28:16 +0300

    Fixed the internationalisation on order/models.py using the gettext_lazy()

--- online_shopping_site/orders/models.py
+++ online_shopping_site/orders/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from decimal import Decimal
 from django.core.validators import MinValueValidator, MaxValueValidator
 from coupons.models import Coupon
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 
 
 class Order(models.Model):
