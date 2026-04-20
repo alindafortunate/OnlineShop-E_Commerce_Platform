79ce6527cefcddf4ef74277622e092ab1240b7ec
alindafortunate <alindafortunate5@gmail.com>
2026-04-20 11:22:11 +0300

    Fixed the django-parler issue that prevents migrations to take effect when you migrate, however it's assumed that it will be worked on in the future versions of django-parler

--- online_shopping_site/shop/admin.py
+++ online_shopping_site/shop/admin.py
@@ -1,19 +1,26 @@
 from django.contrib import admin
+from parler.admin import TranslatableAdmin
 from django.contrib.admin import ModelAdmin
 from .models import Category, Product
 
 
 @admin.register(Category)
-class CategoryAdmin(ModelAdmin):
+class CategoryAdmin(TranslatableAdmin):
     list_display = ["name", "slug"]
-    prepopulated_fields = {"slug": ("name",)}
+
+    def get_prepopulated_fields(self, request, obj=None):
+        return {"slug": ("name",)}
 
 
 @admin.register(Product)
-class ProductAdmin(ModelAdmin):
+class ProductAdmin(TranslatableAdmin):
     list_display = ["name", "slug", "price", "available", "created", "updated"]
     list_filter = ["available", "created", "updated"]
     list_editable = ["price", "available"]
-    prepopulated_fields = {"slug": ("name",)}
+
+    def get_prepopulated_fields(self, request, obj=None):
+        return {"slug": ("name",)}
+
+
 # On this day I posed to first take off a break off the screen for more than 24 hours.
 # From 22/9/2025 at 3:51 pm to 24/9/2025 in the morning hours.
